e9a15f1460fb0bf2444c63ca94ecb43e87b1a9ea
ThiagoSawada <thiagokitajima@gmail.com>
2026-03-04 08:43:09 -0300

    Update main.c

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ double a, b, c, delta, x1, x2;
     scanf("%lf %lf %lf", &a, &b, &c);
 
     if (a == 0) {
-        printf("O coeficiente 'a' deve ser diferente de zero.\n");
+        printf("O (a) deve ser diferente de zero.\n");
     }
 
     delta = (b * b) - (4 * a * c);
